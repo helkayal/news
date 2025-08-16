f57060e19f8a40d4d9642cd650e47cee3bd2e8b2
Hoda Elkayal <helkayal169@gmail.com>
2025-08-16 05:02:12 +0300

    add loading CircularProgressIndicator in article details screen

--- lib/ui/screens/article/full_article.dart
+++ lib/ui/screens/article/full_article.dart
@@ -2,21 +2,48 @@ import 'package:flutter/material.dart';
 import 'package:news/data/model/article.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class FullArticle extends StatelessWidget {
+class FullArticle extends StatefulWidget {
   final Article article;
   const FullArticle({super.key, required this.article});
+
+  @override
+  State<FullArticle> createState() => _FullArticleState();
+}
+
+class _FullArticleState extends State<FullArticle> {
+  late final WebViewController _controller;
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageStarted: (_) {
+            setState(() => _isLoading = true);
+          },
+          onPageFinished: (_) {
+            setState(() => _isLoading = false);
+          },
+          onWebResourceError: (_) {
+            setState(() => _isLoading = false);
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse(widget.article.url ?? ""));
+  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: PageView(
+        body: Stack(
           children: [
-            WebViewWidget(
-              controller: WebViewController()
-                ..setJavaScriptMode(JavaScriptMode.unrestricted)
-                ..loadRequest(Uri.parse(article.url ?? "")),
-            ),
+            PageView(children: [WebViewWidget(controller: _controller)]),
+            if (_isLoading) const Center(child: CircularProgressIndicator()),
           ],
         ),
       ),
